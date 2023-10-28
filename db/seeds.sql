610abfd6ac8b584634ae42903d657a270c831890
Kurt Riecken <kurt.riecken@gmail.com>
2023-10-28 14:15:28 -0400

    seeding to db functioning

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO employee (first_name, last_name)
+VALUES ("Fred", "Johnson"),
+    ("John", "Fredson"),
+    ("Frohn", "Jedson"),
+    ("Pickles", "McGee");
+
+INSERT INTO department (name)
+VALUES ("Exobiology Division"),
+    ("Resource Acquisition Bureau"),
+    ("Ministry of Coffee"),
+    ("Anomaly Analysis Unit");
+
+INSERT INTO role (title, salary)
+VALUES ("Maestro", "300000"),
+    ("Minion", "45000"),
+    ("Trainee", "55000"),
+    ("Engineer", "125000"),
+    ("Maven", "200000");
