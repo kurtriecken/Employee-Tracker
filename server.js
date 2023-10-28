a868c00f293888b1879fc2733dfb9a0727f20572
Kurt Riecken <kurt.riecken@gmail.com>
2023-10-28 13:57:59 -0400

    database created; connection good

--- server.js
+++ server.js
@@ -3,8 +3,28 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3000;
-const app = express;
+const app = express();
 
 // Express middleware
-app.request(express.urlencoded({ extended: false}));
-app.request(expres.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+// Connect to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'rootroot',
+        database: 'company_db'
+    },
+    console.log('Succesfully connect to company_db database!')
+);
+
+// Default response for other requests
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
